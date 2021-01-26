1a1cb8457862dadd9896f6a5d92299f3754dd017
Mekuanint Abebe <se.mekuanint.abebe@gmail.com>
2021-01-26 10:19:45 +0300

    I have done the bank system

---
+++ Banking system/assets/banking.js
@@ -0,0 +1,103 @@
+(function(){
+    let banckAccount =  {
+        accountOwner: " ",
+        accountNum: " ",
+        accountBalance: " ",
+        createAcount: function () {
+            this.accountOwner = prompt("Please insert full name")
+            this.accountNum = prompt("please enter the acount number")
+            this.balance = prompt("please enter the balance deposited")
+            let assume = ((this.balance == (/^[0-9.,]+$/))? "continue Press ok" : "Please enter the valid number")
+            alert(assume);
+            // while(this.accountBalance == (/^[a-z]+$/)){
+            //     alert("you can't insert that number please do again")
+            // }
+        },  
+
+        balance: function(){
+            alert(this.accountBalance)
+        },
+        withdrawAmount: function (accountBalance){
+        var withdrawAmount = prompt("Tell me the amount you wanna to withdraw")
+        var withdraw = ((withdrawAmount >= this.accountBalance)? alert(`You withdraw ${withdrawAmount} and your account balance is now ${this.accountBalance-withdrawAmount}`)
+         : "Please enter another amount you have no that much")
+         alert(withdraw)
+        },
+        Transfer: function(){
+            let accountNumber = prompt("Please enter the account number you wanna transfer")
+            let transferValue = Number(prompt("Enter the amount to transfer"))
+            let transaction = ((transferValue <= this.accountBalance)? alert(`You transferd to account number ${transferValue} to ${accountNumber}`)
+            : "Please enter the valid value")
+            alert(transaction)
+        }
+    }
+    do{
+        var choice = prompt(`please enter from the following
+           1. Create Account
+           2. Withdraw money
+           3. your current 
+           4. Transfer To another account`)
+           if(choice == 1){
+            banckAccount.createAcount();
+           }
+           else if(choice==2){
+            banckAccount.withdrawAmount();
+           }
+           else if(choice==3){
+            banckAccount.balance();
+           }
+           else if(choice==3){
+            banckAccount.balance();
+           }
+           else if(choice==4){
+               banckAccount.Transfer();
+           }
+    }while(choice != 0)
+}())
+
+
+
+
+
+
+// (function(){
+//     let TheChoice = prompt(`
+//                 Please enter your choice from the following functions
+//                         ===========================
+//                         1. Deposite your money
+//                         2. Withdraw your money
+//                         3. See your balance
+//                         4. Transfer to another account
+//                         `);
+//         // using the teranary operator.
+//     var Choice = (
+//         (TheChoice == 1)? createAcount()
+//         :(TheChoice==2)? Withdraw()
+//         :(TheChoice==3)? multiplication()
+//         :(TheChoice==4)? Division()
+//         :(TheChoice==5)? Power()
+//         :(TheChoice==6)? Average()
+//         :'Choose the correct from the choices')
+// }());
+//     var bankAccount =  {
+//         accountOwner: " ",
+//         accountNum: " ",
+//         accountBalance: " "
+//     }
+//     function createAcount(){
+//         bankAccount.accountOwner = prompt("Please insert full name")
+//         bankAccount.accountNum = prompt("please enter the acount number")
+//         bankAccount.accountBalance = prompt("please enter the balance deposited")
+//         alert(`${accountOwner} has ${accountNum} account number and ${balance} balance `)
+//     }
+//     function Withdraw(){
+//       var withdrawAmount = prompt("Tell me the amount you wanna to withdraw")
+//       if(bankAccount.balance < withdrawAmount){
+//           alert("you can't do that please insert a small amount")
+//       }
+//       else{
+//           alert(`You withdraw ${withdrawAmount} and your account balance is ${balance}`)
+//       }
+//     }
+   
+
--- Calculator/assets/js/Calculator.js
+++ Calculator/assets/js/Calculator.js
@@ -1,63 +1,85 @@
-
-let TheChoice = prompt(`
-
+(function(){
+    let TheChoice = prompt(`
                 Please enter your choice from the following functions
                         ===========================
-
-                        1. Addition of Two numbers
-                        2. Substraction of Two numbers
-                        3. Product of Two numbers
-                        4. division of Two numbers
+                        1. Addition of numbers
+                        2. Substraction of two numbers
+                        3. Product of numbers
+                        4. division of two numbers
                         5. Power of Two numbers
-                        `)
-if(TheChoice == 1){
+                        6. average of two numbers
+                        7. maximum of the two numbers
+                        8. minimum the two numbers
+                        `);
+        // using the teranary operator.
+    var Choice = (
+        (TheChoice == 1)? addition()
+        :(TheChoice==2)? substraction()
+        :(TheChoice==3)? multiplication()
+        :(TheChoice==4)? Division()
+        :(TheChoice==5)? Power()
+        :(TheChoice==6)? Average()
+        :'Choose the correct from the choices')
+        console.log(Choice)
+}());
+function addition(){
+    var arrayLength = prompt("Please enter the size of numbers you wanna calculate");
+    var sum = 0;
+    for (i=0; i < arrayLength; i++){
+        var x = Number(window.prompt("please enter " + i + "element"))
+        sum+=x
+ }
+    window.Number(alert(sum))
+}
+// substraction function
+function substraction(){
     var first = Number(prompt("enter the first number"))
     var second = Number(prompt("enter the second number"))
-    function addition(){ 
-        var summ = first + second;
-        return summ
-    }
-    Number(alert(addition()))
+    var difference = first -second
+    window.Number(alert(difference))
+ }
+//  for the multiplication
+function multiplication(){
+    var arrayLength = prompt("please the size of numbers you wanna to calculate");
+    var product = 1;
+    for (i=0; i < arrayLength; i++){
+        var x = Number(window.prompt("please enter " + i + "element"))
+        product+=x
+ }
+    window.Number(alert(product))
 }
-else if(TheChoice == 2){
+function Division(){
     var first = Number(prompt("enter the first number"))
     var second = Number(prompt("enter the second number"))
-    function Substraction(){ 
-        var answer = first - second;
-        return answer
+    if (second == 0){
+        alert("Division by zero is not possible")
+    }else{
+        var answer = first / second;
     }
-    Number(alert(Substraction()))
+    window.Number(alert(answer))
+ }
+ function Power(){
+     var num = Number(prompt("Enter the number you wanna square"))
+     var power = Number(prompt("Enter the power"))
+     var answer = Math.pow(num, power)
+     window.Number(alert(answer))
+ }
+ function Average(){
+    var arrayLength = prompt("please the size of numbers you wanna to calculate");
+    let summ = 0
+    let average = 0
+    for (i=0; i < arrayLength; i++){
+        var x = Number(window.prompt("please enter " + i + "element"))
+        summ += x;
+        average = summ/arrayLength
+   }
+    window.Number(alert(average))
 }
-else if(TheChoice == 3){
-    var first = Number(prompt("enter the first number"))
-    var second = Number(prompt("enter the second number"))
-    function Product(){ 
-        var answer = first * second;
-        return answer
-    }
-    Number(alert(Product()))
-}
-else if(TheChoice == 4){
-    var first = Number(prompt("enter the first number"))
-    var second = Number(prompt("enter the second number"))
-    function Division(){ 
-        if (second == 0){
-            alert("Division by zero is not possible")
-        }else{
-            var answer = first / value;
-        }
-        return answer
-    }
-    Number(alert(Division()))
-}
-else if(TheChoice == 5){
-    var first = Number(prompt("enter the first number"))
-    var second = Number(prompt("enter the second number"))
-    function power(){ 
-        var answer = first ** second;
-        return answer
-    }
-    Number(alert(power()))
-}
+
+
+
+
+
+
 
 
