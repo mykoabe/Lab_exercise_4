b04ed41556e6d168357548e36c15d2c69c33c7ba
Mekuanint Abebe <se.mekuanint.abebe@gmail.com>
2021-01-27 13:08:28 +0300

    I have edited the banking part

--- Banking system/assets/js/bankings.js
+++ Banking system/assets/js/bankings.js
@@ -1,5 +1,6 @@
 var myData = [];
 var myData2 = [];
+// the immidiate invoking functions
 (function(){
     do{
         var choice = prompt(`please enter from the following
@@ -7,7 +8,8 @@ var myData2 = [];
            2. another account
            3. Account balance 
            4. withdraw
-           5. Transfer to another account`);
+           5. Transfer to another account
+           6. display`);
            if(choice == 1){
              creatFirstAccount();
            }
@@ -22,6 +24,9 @@ var myData2 = [];
            }
            else if(choice==5){
             transfer();
+           }
+           else if(choice==6){
+            display();
            }
     }while(choice != 0)
 }());
@@ -57,15 +62,19 @@ function withdraw(){
         alert("You are at the small amount if it is less than 400 you can't withdraw")
     } 
 }
+function display(){
+    alert(myData2)
+}
+
 function transfer(){
-    let transferAccunt = prompt("Please insert the account number you wanna transfer")
-    if (parseFloat(transferAccunt) === myData2[1]){
+    var transferAccunt = prompt("Please insert the account number you wanna transfer")
+    if (parseFloat(transferAccunt) === parseFloat(myData2[1])){
         let transferValue = prompt("Please enter the amount you want to transfer")
         if(parseFloat(transferValue) <= myData2[1]){
             myData2[2] = myData2[2] + transferValue
+            alert(`You transfered ${transferValue} to ${myData2[1]}
+            and ${myData[1]} have a balance of ${(myData[2] - transferValue)}`)
         }
-        alert(`You transfered ${transferValue} to ${myData2[1]}
-        and ${myData[1]} have a balance of ${(myData[2] - transferValue)}`)
     }else{
         alert("you can't transfer")
     }
